e546faca8b5448948c8a7f62c285291dcb14f982
dm13450 <dean.markwick@talk21.com>
2018-04-12 19:32:55 +0100

    Dirichlet Hawkes

--- src/HawkesProcess.jl
+++ src/HawkesProcess.jl
@@ -7,7 +7,7 @@ module HawkesProcess
     include("event_difference_matrix.jl")
     include("count_event.jl")
     include("all_constant_hawkes.jl")
-    #include("dirichlet_background_hawkes.jl")
+    include("dirichlet_background_hawkes.jl")
 
     export HawkesFit
 
