19040f444638e29f1b52664fe5efe636c052db7b
dm13450 <dean.markwick@talk21.com>
2018-06-07 15:09:38 +0100

    Parent Sample Test for Hierarchical Hawkes and spelling correction

--- test/parent_sample_test.jl
+++ test/parent_sample_test.jl
@@ -1,27 +1,37 @@
 using HawkesProcess, Base.Test, Distributions
 
+### Test Data
 testEventTimes = cumsum(rand(50))
 testDiff = HawkesProcess.eventDifferenceMatrix(testEventTimes)
-
 expObj = Exponential(0.5)
 kernelTest(x) = pdf.(expObj, x)
 bgFunction(x) = 0.5 * x
 
-testParentSampleCol_constant = HawkesProcess.parentSampleCol(testDiff[:,1], 0.5, 0.5, kernelTest)
-testParentSampleCol_function = HawkesProcess.parentSampleCol(testDiff[:,1], bgFunction, 0.5, kernelTest)
 
+### Test Constant
+testParentSampleCol_constant = HawkesProcess.parentSampleCol(testDiff[:,1], 0.5, 0.5, kernelTest)
 testParentSample_constant = HawkesProcess.parentSample(testDiff, 0.5, 0.5, kernelTest)
-testParentSample_function = HawkesProcess.parentSample(testDiff, bgFunction, 0.5, kernelTest)
 
 @test isa(testParentSampleCol_constant, Int64)
-@test isa(testParentSampleCol_function, Int64)
-
 @test isa(testParentSample_constant, Array)
-@test isa(testParentSample_function, Array)
-
 @test size(testParentSample_constant) == (50, )
+
+### Test Function
+testParentSampleCol_function = HawkesProcess.parentSampleCol(testDiff[:,1], bgFunction, 0.5, kernelTest)
+testParentSample_function = HawkesProcess.parentSample(testDiff, bgFunction, 0.5, kernelTest)
+
+@test isa(testParentSampleCol_function, Int64)
+@test isa(testParentSample_function, Array)
 @test size(testParentSample_function) == (50, )
 
+### Test Hierarchical
 
+testParentSample_hier = map( x -> HawkesProcess.parentSample(x,
+                                                 0.5,
+                                                 0.5,
+                                                 kernelTest), (testDiff, testDiff))
 
-
+@test isa(testParentSample_hier, Tuple)
+@test isa(testParentSample_hier[1], Array)
+@test isa(testParentSample_hier[2], Array)
+@test length(testParentSample_hier) == 2
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,8 @@ using Base.Test
 
 println("Starting Tests")
 
-@time @testset "Event Differnence Matrix Tests" begin include("event_difference_matrix_test.jl") end
-@time @testset "Parent Sample Column Test" begin include("parent_sample_test.jl") end
+@time @testset "Event Difference Matrix Tests" begin include("event_difference_matrix_test.jl") end
+@time @testset "Parent Sample Tests" begin include("parent_sample_test.jl") end
 @time @testset "Event Counts Test" begin include("count_event_test.jl") end
 @time @testset "Basic Hawkes Fit Test" begin include("all_constant_hawkes_test.jl") end
 @time @testset "Dirichlet Backgound  Hawkes Fit Test" begin include("dirichlet_background_hawkes_test.jl") end
