e677cd8cf59d8857147ea95b953725ba76d4ef07
dm13450 <dean.markwick@talk21.com>
2020-05-16 15:19:27 +0100

    Update travis

--- src/simulate.jl
+++ src/simulate.jl
@@ -2,6 +2,7 @@
 function simulate(bg::Number, kappa::Float64, kern::Function, maxT::Number)
 
     childfunction(x) = kappa * kern(x)
+    lambda_max = childfunction(0)
     backgroundEvents = poisson_simulation(bg, maxT)
 
     eventbus = backgroundEvents
@@ -9,7 +10,7 @@ function simulate(bg::Number, kappa::Float64, kern::Function, maxT::Number)
 
     while !isempty(eventbus)
         simevent = popfirst!(eventbus)
-        newevents = poisson_simulation(childfunction, maxT - simevent)
+        newevents = poisson_simulation(childfunction, maxT - simevent, lambda_max)
         append!(eventbus, newevents)
         if !isempty(newevents)
             append!(allevents, newevents .+ simevent)
@@ -26,6 +27,10 @@ end
 
 function poisson_simulation(rate::Function, maxT::Number)
     lambda_max = 1.1*maximum(rate(range(0, maxT, length=1000)))
+    poisson_simulation(rate, maxT, lambda_max)
+end
+
+function poissson_simulation(rate::Function, maxT::Number, lambda_max::Number)
     tstar = poisson_simulation(lambda_max, maxT)
     accept_prob = rate(tstar) / lambda_max
     tstar[rand(length(accept_prob)) .< accept_prob]
