293f22f624a2e2effa01d02ac4dfb0ae978fe961
dm13450 <dean.markwick@talk21.com>
2020-07-16 08:13:06 +0100

    Expand tests

--- test/simulate_test.jl
+++ test/simulate_test.jl
@@ -8,7 +8,10 @@ using Distributions
     end
     @testset "Hawkes Simulation" begin
         kern(x) = pdf.(Distributions.Exponential(1/0.5), x)
-        testevents = HawkesProcesses.simulate(0.5, 0.5, kern, 1)
+        testevents = HawkesProcesses.simulate(0.5, 0.5, kern, 10)
+
+        @test length(testevents) > 0
+
         inferTests && @inferred HawkesProcesses.simulate(0.5, 0.5, kern, 100)
     end
 
